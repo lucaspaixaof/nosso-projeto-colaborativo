d6b6e942074750083cc51ccaef77d6fa7d203b84
Jean-XIII <jeanoliveiraxiii@gmail.com>
2025-12-11 10:42:13 -0300

    Formatado o texto de faturamento.py

--- faturamento.py
+++ faturamento.py
@@ -6,16 +6,15 @@ lucro = faturamento - custo
 
 
 
-print('O lucro foi de', lucro)
-print('O lucro foi de', lucro)
+print('O lucro foi de R$',lucro)
 
 margem_lucro = custo / faturamento
 
-print("a margem de lucro é de", margem_lucro)
+print(f"A margem de lucro é de R${margem_lucro:.2f}")
 
 novas_vendas = 5000
 taxa = 0.05
 
 valortotal = novas_vendas - (novas_vendas * taxa)
 print('Surgiram novas vendas com uma taxa de 5%')
-print('Total de:', valortotal)
+print('Total de R$', valortotal)
