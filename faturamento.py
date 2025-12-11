ce20a8ab227f9683a0c78a986beabf2c4e1c7c00
Jailsonsalima <jailsonsalima99@gmail.com>
2025-12-11 10:45:22 -0300

    Update faturamento.py
    inserido taxa

--- faturamento.py
+++ faturamento.py
@@ -3,9 +3,17 @@
 faturamento = 1200
 custo = 700 
 lucro = faturamento - custo
+vendas = 5000
+taxa = 0.1
 
+
+    
 print('O lucro foi de', lucro)
 
 margem_lucro = custo / faturamento
 
 print("a margem de lucro é de", margem_lucro)
+
+if(vendas >= 5000):
+    margem_lucro -= taxa
+    print('Como sua venda passou da isenção então seu lucro foi apenas de: ', margem_lucro)
