5617401885cbc954f032721c97f81e34e2aa48fa
amaues27 <amaues123@gmail.com>
2025-12-11 11:15:26 -0300

    Update faturamento.py

--- faturamento.py
+++ faturamento.py
@@ -18,3 +18,7 @@ taxa = 0.05
 valortotal = novas_vendas - (novas_vendas * taxa)
 print('Surgiram novas vendas com uma taxa de 5%')
 print('Total de R$', valortotal)
+
+# add uma nova lista de valores (no exemplo é a receita diaria) e fiz a soma total dos valores  
+lista_vendas_diarias = [1500,850,960,750]
+print(f" A receita diária é {sum(lista_vendas_diarias):.2f}")
